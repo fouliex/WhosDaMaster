4abbdc85ba5761bc1f03c0c7e0746c4bae9601e1
George Fouche <fouliex@gmail.com>
2018-10-11 09:01:11 -0700

    add longest substring

--- src/com/practice/leetcode/LongestSubstringWithoutRepeatingCharacters.java
+++ src/com/practice/leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,7 +1,35 @@
 package com.practice.leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
+ * Given a string, find the length of the longest substring without repeating characters.
+ *
  * Created by George Fouche on 10/8/18.
  */
 public class LongestSubstringWithoutRepeatingCharacters {
+
+    public static void main(String[] args) {
+       LongestSubstringWithoutRepeatingCharacters  longestSubstringWithoutRepeatingCharacters = new LongestSubstringWithoutRepeatingCharacters();
+        System.out.println(longestSubstringWithoutRepeatingCharacters.longestSubsString("abcbb"));
+    }
+
+    private int longestSubsString(String str) {
+        Set<Character> set = new HashSet<Character>();
+        int ans =0;
+        int i =0;
+        int j =0;
+
+        while (i < str.length() && j < str.length()){
+            if(!set.contains(str.charAt(j))){
+                set.add(str.charAt(j++));
+                ans = Math.max(ans,j-i);
+            }else{
+                set.remove(str.charAt(i++));
+            }
+        }
+        return ans;
+
+    }
 }
