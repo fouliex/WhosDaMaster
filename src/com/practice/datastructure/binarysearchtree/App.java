ad5a17c2c01097e36477db336d0dc90496c8d60c
George Fouche <fouliex@gmail.com>
2018-11-02 11:07:33 -0700

    Working on Binary Search Tree

---
+++ src/com/practice/datastructure/binarysearchtree/App.java
@@ -0,0 +1,12 @@
+package com.practice.datastructure.binarysearchtree;
+
+/**
+ * Created by George Fouche on 11/1/18.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        BST tree = new BST();
+        tree.insert(10,"Ten");
+    }
+}
---
+++ src/com/practice/datastructure/binarysearchtree/Node.java
@@ -0,0 +1,16 @@
+package com.practice.datastructure.binarysearchtree;
+
+/**
+ * Created by George Fouche on 11/1/18.
+ */
+public class Node {
+    int key;
+    String value;
+    Node leftChild;
+    Node rightChild;
+
+    public Node(int key, String value) {
+        this.key = key;
+        this.value =value;
+    }
+}
