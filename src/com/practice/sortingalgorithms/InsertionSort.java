a4b644f05d2a480f95c00f16363b70588c4f1f6d
George Fouche <fouliex@gmail.com>
2018-11-05 13:58:59 -0800

    Add Insertion and Selection algorithms

---
+++ src/com/practice/sortingalgorithms/InsertionSort.java
@@ -0,0 +1,24 @@
+package com.practice.sortingalgorithms;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] myArray = insertionSort(new int[]{9, 8, 3, 13, 87, 12, 99});
+        for (int i : myArray) {
+            System.out.println(i);
+        }
+
+    }
+
+    private static int[] insertionSort(int[] a) {
+        for (int i = 1; i< a.length;i++){
+            int element = a[i];
+            int j = i-1;
+            while (j>= 0 && a[j]>element){
+                a[j+1] = a[j];
+                j--;
+            }
+             a[j+1] = element;
+        }
+        return a;
+    }
+}
