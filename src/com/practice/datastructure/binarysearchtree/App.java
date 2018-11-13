8477ed1ed1ce026b28658e77c97292fbd609f5c8
George Fouche <fouliex@gmail.com>
2018-11-13 08:07:11 -0800

    Working on Binary search Tree

--- src/com/practice/datastructure/binarysearchtree/App.java
+++ src/com/practice/datastructure/binarysearchtree/App.java
@@ -8,5 +8,12 @@ public class App {
     public static void main(String[] args) {
         BST tree = new BST();
         tree.insert(10,"Ten");
+        tree.insert(20,"twenty");
+        tree.insert(15,"fifteen");
+
+        System.out.println(tree.findMin().key);
+        System.out.println(tree.findMax().key);
+
+        // In Order sucessor
     }
 }
--- src/com/practice/datastructure/binarysearchtree/BST.java
+++ src/com/practice/datastructure/binarysearchtree/BST.java
@@ -8,30 +8,112 @@ public class BST {
     private Node root;
 
     public void insert(int key, String value) {
-        Node newNode = new Node(key,value);
+        Node newNode = new Node(key, value);
 
-        if(root == null){
+        if (root == null) {
             root = newNode;
-        }else{
+        } else {
             Node current = root;
             Node parent;
 
-            while(true){
+            while (true) {
                 parent = current;
-                if(key < current.key){
+                if (key < current.key) {
                     current = current.leftChild;
-                    if(current == null){
-                        parent.leftChild=newNode;
+                    if (current == null) { // It's parent is the leaf node
+                        parent.leftChild = newNode;
                         return;
                     }
-                }else{
-                    current =current.rightChild;
-                    if(current==null){
-                        parent.rightChild=newNode;
+                } else {
+                    current = current.rightChild;
+                    if (current == null) {
+                        parent.rightChild = newNode;
                         return;
                     }
                 }
             }
         }
     }
+
+    public Node findMin() {
+        Node current = root;
+        Node last = null;
+
+        while (current != null) {
+            last = current;
+            current = current.leftChild;
+
+        }
+
+        return last;
+    }
+
+    public Node findMax() {
+        Node current = root;
+        Node last = null;
+
+        while (current != null) {
+            last = current;
+            current = current.rightChild;
+
+        }
+        return last;
+    }
+
+    public boolean remove(int key) {
+
+        Node currentNode = root;
+        Node parentNode = root;
+
+
+        boolean isLeftChild = false;
+
+        //Searching to  find the node with the key to delete
+        while (currentNode.key != key) {
+            parentNode = currentNode;
+            if (key < currentNode.key) {
+                isLeftChild = true;
+                currentNode = currentNode.leftChild;
+            } else {
+                currentNode = currentNode.rightChild;
+                isLeftChild = false;
+            }
+            if (currentNode == null) {
+                return false;
+            }
+
+        }
+
+        //found the node
+        Node nodeToDelete = currentNode;
+
+        // if node is a leaf
+        if (nodeToDelete.leftChild == null && nodeToDelete.rightChild == null) {
+            if (nodeToDelete == root) {
+                root = null;
+            } else if (isLeftChild) {
+                parentNode.leftChild = null;
+            } else {
+                parentNode.rightChild = null;
+            }
+        }
+
+        //  if a node has a child
+        else if (nodeToDelete.rightChild == null) {
+            if (nodeToDelete == root) {
+                root = nodeToDelete.leftChild;
+            } else if (isLeftChild) {
+                parentNode.leftChild = nodeToDelete.leftChild;
+            } else {
+                parentNode.rightChild = nodeToDelete.leftChild;
+            }
+        }
+
+        // if node 2 children
+
+
+        return true;
+    }
+
+
 }
