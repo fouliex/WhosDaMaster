d0eb6b71c5e95fea8fa16e793baffe0ae8a0f69e
George Fouche <fouliex@gmail.com>
2018-11-29 13:53:25 -0800

    doing arrays problems

---
+++ src/com/practice/leetcode/array/RemoveDuplicateFromSortedArray.java
@@ -0,0 +1,52 @@
+package com.practice.leetcode.array;
+
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.Set;
+
+/**
+ * Given a sorted array nums, remove the duplicates in-place
+ * such that each element appear only once and return the new length.
+ * Leetcode: https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+ * Created by George Fouche on 11/26/18.
+ */
+public class RemoveDuplicateFromSortedArray {
+
+    public static void main(String[] args) {
+        int[] array1 = {1, 1, 2};
+        int[] array2 = {0,0,1,1,1,2,2,3,3,4};
+
+        System.out.println(removeDuplicates(array1));
+        System.out.println(removeDuplicates(array2));
+        System.out.println();
+        System.out.println(removeDuplicates2(array1));
+        System.out.println(removeDuplicates2(array2));
+    }
+
+    private static int removeDuplicates(int[] array1) {
+
+        if(array1 == null || array1.length == 0){
+            return 0;
+        }
+
+        // 2 pointers technique
+        int i = 0;
+        for (int j = 0; j < array1.length; j++) {
+            if (array1[j] != array1[i]) {
+                i++;
+                array1[i] = array1[j];
+            }
+        }
+        return i + 1;
+    }
+
+
+    private static int removeDuplicates2(int[] nums){
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums ){
+            set.add(num);
+        }
+
+        return set.size();
+    }
+}
---
+++ src/com/practice/leetcode/array/TwoSum.java
@@ -0,0 +1,7 @@
+package com.practice.leetcode.array;
+
+/**
+ * Created by George Fouche on 11/27/18.
+ */
+public class TwoSum {
+}
