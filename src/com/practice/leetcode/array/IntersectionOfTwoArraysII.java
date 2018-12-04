50e31057366b88e02777df5885c061a2e85bb941
George Fouche <fouliex@gmail.com>
2018-12-03 20:11:48 -0800

    doing arrays problems

---
+++ src/com/practice/leetcode/array/IntersectionOfTwoArraysII.java
@@ -0,0 +1,41 @@
+package com.practice.leetcode.array;
+
+import java.util.*;
+
+/**
+ * Leetcode: https://leetcode.com/problems/intersection-of-two-arrays-ii/
+ * Created by George Fouche on 12/3/18.
+ */
+public class IntersectionOfTwoArraysII {
+
+    public static void main(String[] args) {
+
+        int[] nums1 =  {4,9,5};
+        int [] nums2 =  {9,4,9,8,4};
+        System.out.println(Arrays.toString(intersection(nums1,nums2)));
+
+
+    }
+
+    @SuppressWarnings("Duplicates")
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        List list = new ArrayList();
+        List<Integer> resultList = new ArrayList<>();
+        for (int num : nums1) {
+            list.add(num);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            if (list.contains(nums2[i])) {
+                resultList.add(nums2[i]);
+                list.remove(new Integer(nums2[i])); //index
+            }
+        }
+        int[] result = new int[resultList.size()];
+        int i = 0;
+        for (Integer num : resultList) {
+            result[i++] = num;
+        }
+
+        return result;
+    }
+}
--- src/com/practice/leetcode/array/SingleNumber.java
+++ src/com/practice/leetcode/array/SingleNumber.java
@@ -1,7 +1,34 @@
 package com.practice.leetcode.array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by George Fouche on 11/27/18.
  */
 public class SingleNumber {
+
+    public static void main(String[] args) {
+        int[] nums =  {2,2,1};
+        System.out.println(singleNumber(nums));
+    }
+    public static int singleNumber(int[] nums){
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i =0 ; i < nums.length; i++){
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i], 1);
+
+            }else{
+               map.put(nums[i],map.get(nums[i])+1);
+            }
+        }
+        for( Integer num : nums){
+            if(map.get(num) == 1){
+                return num;
+            }
+        }
+        return 0;
+    }
+
 }
