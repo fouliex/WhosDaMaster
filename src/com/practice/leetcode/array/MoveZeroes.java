78f5ada7e4777f76b15f8d8646bfd36804f85131
George Fouche <fouliex@gmail.com>
2018-12-05 16:47:49 -0800

    Add 2 array problems

--- src/com/practice/leetcode/array/MoveZeroes.java
+++ src/com/practice/leetcode/array/MoveZeroes.java
@@ -1,7 +1,35 @@
 package com.practice.leetcode.array;
 
+import java.util.Arrays;
+
 /**
+ *Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ * Leetcode: https://leetcode.com/problems/move-zeroes/
  * Created by George Fouche on 11/27/18.
  */
 public class MoveZeroes {
+    public static void main(String[] args) {
+        int [] nums = {0,1,0,3,12};
+        System.out.println(Arrays.toString(moveZeroes(nums)));
+
+    }
+    public static int[] moveZeroes(int[] nums) {
+        int count = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                nums[count] = nums[i];
+                count++;
+
+            }
+        }
+        while(count < nums.length){
+            nums[count] = 0;
+            count++;
+        }
+
+        return nums;
+
+    }
+
 }
--- src/com/practice/leetcode/array/TwoSum.java
+++ src/com/practice/leetcode/array/TwoSum.java
@@ -1,7 +1,32 @@
 package com.practice.leetcode.array;
 
+import java.util.*;
+
 /**
+ *
+ * Leetcode: https://leetcode.com/problems/two-sum/
  * Created by George Fouche on 11/27/18.
  */
 public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+
+        System.out.println(Arrays.toString(twoSums(nums,9)));
+        
+    }
+
+    private static int[] twoSums(int[] nums, int target) {
+        List<Integer> map = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++){
+            int value = target - nums[i];
+            if(map.contains(value)){
+                return new int[]{nums[i],value};
+            }else{
+                map.add(nums[i]);
+            }
+        }
+        return new int[0];
+    }
+
 }
